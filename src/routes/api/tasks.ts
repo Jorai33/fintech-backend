2c5eb04fbb2fed760796feb0778706043fc4d9bc
vita <vita@kataev.pro>
2019-03-24 15:56:49 +0300

    Implement toggle task method

--- src/routes/api/tasks.ts
+++ src/routes/api/tasks.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
 import {state} from "../..";
-import {formatNotFound} from "../../utils";
+import {formatNotFound, saveTasks} from "../../utils";
 import {api} from "./api";
 
 api.get('/tasks', (req: Request, res: Response) => {
@@ -16,4 +16,19 @@ api.get('/tasks/:id', (req: Request, res: Response) => {
     }
 
     res.json(task);
+});
+
+api.post('/tasks/toggle', async (req: Request, res: Response) => {
+    const taskId = req.body.taskId;
+    const task = state.tasks.find(task => task.id === taskId);
+    if (!task) {
+        return res.status(400).json({
+            error: 400,
+            message: `Task id ${taskId} not found!`
+        });
+    }
+
+    task.completed = !task.completed;
+    await saveTasks(state.tasks);
+    res.json({success: true});
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,7 @@
 import {IError} from "./models/error";
+import {ITask} from "./models/task";
+import * as fs from 'fs';
+import * as path from 'path';
 
 export function getUUID(): string {
     return Math.random().toString(32).substring(2);
@@ -9,4 +12,16 @@ export function formatNotFound(id: string, message = `${id} not found`): IError 
         code: 404,
         message
     }
+}
+
+export async function saveTasks(tasks: ITask[]) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path.join(__dirname, '../../db/tasks.json'), tasks, function (error) {
+            if (error) {
+                return reject(error);
+            }
+
+            resolve()
+        });
+    })
 }
